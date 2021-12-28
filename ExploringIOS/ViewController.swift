519f46030664b38e0ae0b8d359bfc05be1b6b63e
Shyam Shankar <shyam.shankar.s@gmail.com>
2021-12-27 17:14:51 -0800

    First working version

--- ExploringIOS/ViewController.swift
+++ ExploringIOS/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var lblOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func onSubmit(_ sender: UIButton) {
+        if txtInput.text == "" {
+            lblOutput.text = "Error: Please enter some text!"
+        } else {
+            lblOutput.text = txtInput.text
+        }        
+    }
 }
 
