32850db171d48e47d8599472e6285141b2996e72
Shyam Shankar <shyam.shankar.s@gmail.com>
2021-12-27 21:31:48 -0800

    Added ability to take a picture and display it; Tested it on my phone

--- ExploringIOS/ViewController.swift
+++ ExploringIOS/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var txtInput: UITextField!
     @IBOutlet weak var lblOutput: UILabel!
     
@@ -23,7 +24,27 @@ class ViewController: UIViewController {
             lblOutput.text = "Error: Please enter some text!"
         } else {
             lblOutput.text = txtInput.text
-        }        
+        }
+    }
+    
+    @IBAction func onTakePicture(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = true
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    // Implementing UIImagePickerControllerDelegate::imagePickerController
+    // onTakePicture configures "self" which implements the delegate to handle camera capture completion.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imgView.contentMode = .scaleAspectFit
+            imgView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 }
 
